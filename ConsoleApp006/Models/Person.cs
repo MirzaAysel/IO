e8ec4650eb6fae12f63bade5e8fe8dade8ec0ea9
MirzaAysel <99787066+MirzaAysel@users.noreply.github.com>
2022-04-06 05:12:01 +0400

    Add files via upload

---
+++ ConsoleApp006/Models/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp006.Models
+{
+    internal class Person
+    {
+        public string Name { get; set; }
+        public string Surname {get;set;}
+        public int Id { get; }
+        private static int _id;
+        static Person()
+        {
+            _id = 0;
+        }
+        private  Person()
+        {
+            Id = ++_id;
+        }
+        public Person(string name, string surname):this()
+        {
+            Name = name;
+            Surname = surname;
+
+        }
+
+    }
+}
